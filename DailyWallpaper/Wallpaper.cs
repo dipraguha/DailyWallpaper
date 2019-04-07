e5abb06fd69214a2fefc43edd86360e3bc156ad1
Dipra Guha <dipra.g.n@gmail.com>
2019-04-07 13:23:57 +0800

    Refactored SetWallpaper method

--- DailyWallpaper/Wallpaper.cs
+++ DailyWallpaper/Wallpaper.cs
@@ -27,7 +27,7 @@ namespace DailyWallpaper
             Center
         }
 
-        public static void SetWallpaper(Uri url, Style style)
+        public static Image GetImageFromUrl(Uri url)
         {
             try
             {
@@ -41,58 +41,79 @@ namespace DailyWallpaper
                             {
                                 using (var img = Image.FromStream(stream))
                                 {
-                                    if (img != null)
-                                    {
-                                        string tempPath = Path.Combine(Path.GetTempPath(), "Wallpaper.jpg");
-                                        img.Save(tempPath, ImageFormat.Jpeg);
-
-                                        RegistryKey regKey = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
-
-                                        switch (style)
-                                        {
-                                            case Style.Center:
-                                                regKey.SetValue(@"WallpaperStyle", "0");
-                                                regKey.SetValue(@"TileWallpaper", "0");
-                                                break;
-                                            case Style.Fill:
-                                                regKey.SetValue(@"WallpaperStyle", "10");
-                                                regKey.SetValue(@"TileWallpaper", "0");
-                                                break;
-                                            case Style.Fit:
-                                                regKey.SetValue(@"WallpaperStyle", "6");
-                                                regKey.SetValue(@"TileWallpaper", "0");
-                                                break;
-                                            case Style.Span:
-                                                regKey.SetValue(@"WallpaperStyle", "22");
-                                                regKey.SetValue(@"TileWallpaper", "0");
-                                                break;
-                                            case Style.Stretch:
-                                                regKey.SetValue(@"WallpaperStyle", "2");
-                                                regKey.SetValue(@"TileWallpaper", "0");
-                                                break;
-                                            case Style.Tile:
-                                                regKey.SetValue(@"WallpaperStyle", "0");
-                                                regKey.SetValue(@"TileWallpaper", "1");
-                                                break;
-                                            default:
-                                                break;
-                                        }
-
-                                        int result = SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, tempPath, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
-                                        if (result == 0)
-                                        {
-                                            EventLogger.LogEvent("Setting wallpaper failed. SystemParametersInfo returned 0.");
-                                        }
-                                    }
-                                    else
-                                    {
-                                        EventLogger.LogEvent("Setting wallpaper failed. Image is null.");
-                                    }
+                                    return img;
                                 }
                             }
                         }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                EventLogger.LogEvent(e.Message);
+                return null;
+            }
+        }
+
+        public static void SetWallpaper(Uri url, Style style)
+        {
+            try
+            {
+                var img = GetImageFromUrl(url);
+
+                if (img != null)
+                {
+                    string tempPath = Path.Combine(Path.GetTempPath(), "Wallpaper.jpg");
+                    img.Save(tempPath, ImageFormat.Jpeg);
+
+                    RegistryKey regKey = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+
+                    switch (style)
+                    {
+                        case Style.Center:
+                            regKey.SetValue(@"WallpaperStyle", "0");
+                            regKey.SetValue(@"TileWallpaper", "0");
+                            break;
+                        case Style.Fill:
+                            regKey.SetValue(@"WallpaperStyle", "10");
+                            regKey.SetValue(@"TileWallpaper", "0");
+                            break;
+                        case Style.Fit:
+                            regKey.SetValue(@"WallpaperStyle", "6");
+                            regKey.SetValue(@"TileWallpaper", "0");
+                            break;
+                        case Style.Span:
+                            regKey.SetValue(@"WallpaperStyle", "22");
+                            regKey.SetValue(@"TileWallpaper", "0");
+                            break;
+                        case Style.Stretch:
+                            regKey.SetValue(@"WallpaperStyle", "2");
+                            regKey.SetValue(@"TileWallpaper", "0");
+                            break;
+                        case Style.Tile:
+                            regKey.SetValue(@"WallpaperStyle", "0");
+                            regKey.SetValue(@"TileWallpaper", "1");
+                            break;
+                        default:
+                            break;
+                    }
+
+                    int result = SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, tempPath, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+                    if (result == 0)
+                    {
+                        EventLogger.LogEvent("Setting wallpaper failed. SystemParametersInfo returned 0.");
                     }
                 }
+                else
+                {
+                    EventLogger.LogEvent("Setting wallpaper failed. Image is null.");
+                }
+
             }
             catch (Exception e)
             {
