caf75698433d998525898fac104e42f91e7d1e21
Dipra Guha <dipra.g.n@gmail.com>
2019-07-07 20:10:36 +0800

    Changed return type void to Task for async method

--- DailyWallpaper/ImageSource.cs
+++ DailyWallpaper/ImageSource.cs
@@ -131,7 +131,7 @@ namespace DailyWallpaper
             }
         }
 
-        private async static void TriggerUnsplashImageDownloadCounterEndpointAsync(string url, string appKey)
+        private async static Task TriggerUnsplashImageDownloadCounterEndpointAsync(string url, string appKey)
         {
             try
             {
